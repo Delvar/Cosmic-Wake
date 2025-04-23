952c1a53685fed85065924143638ee48f7926e16
Morgan Gilroy <morgan.gilroygmai.com>
2025-04-23 17:26:07 +0100

    Rescaled Sol, see if htis feels better.

--- src/galaxy.js
+++ src/galaxy.js
@@ -13,28 +13,35 @@ import { TWO_PI } from './utils.js';
 export function createGalaxy() {
     const randomAngle = () => Math.random() * TWO_PI;
     const subtypes = celestialTypes['planet'].subtypes;
-
-    let stars = [new Star(0, 100, new Colour(1, 1, 0), null, 0, celestialTypes['star'], null, 'Sun', null, null)];
+    const au = 4000;
+    const planetScale = 0.008;
+    let stars = [new Star(0, 300, new Colour(1, 1, 0), null, 0, celestialTypes['star'], null, 'Sun', null, null)];
 
     /* distance, radius, color, parent = null, angle = 0, type = celestialTypes['planet'], subtype = null, name = '', starSystem = null, ring = null */
     let planets = [
-        new Planet(800, 20, new Colour(0.55, 0.27, 0.07), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Iron'], 'Mercury', null, null),
-        new Planet(1400, 30, new Colour(1, 0.84, 0), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Terrestrial'], 'Venus', null, null),
-        new Planet(2000, 34, new Colour(0, 0.72, 0.92), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Terrestrial'], 'Earth', null, null),
-        new Planet(2800, 24, new Colour(1, 0.27, 0), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Terrestrial'], 'Mars', null, null),
-        new Planet(4000, 60, new Colour(0.85, 0.65, 0.13), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Gas Giant'], 'Jupiter', null, null),
-        new Planet(5600, 50, new Colour(0.96, 0.64, 0.38), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Gas Giant'], 'Saturn', null,
-            new PlanetaryRing(1.5, 2.5, new Colour(0.96, 0.71, 0.51, 0.75))),
-        new Planet(7200, 40, new Colour(0.53, 0.81, 0.92), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Ice Giant'], 'Uranus', null,
-            new PlanetaryRing(1.8, 2.0, new Colour(0.6, 0.9, 1.0, 0.25))),
-        new Planet(8000, 40, new Colour(0, 0, 0.55), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Ice Giant'], 'Neptune', null,
-            new PlanetaryRing(2.3, 2.5, new Colour(0.7, 0.7, 0.8, 0.1)))
+        new Planet(0.39 * au, 4879 * planetScale, new Colour(0.55, 0.27, 0.07), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Iron'], 'Mercury', null, null),
+        new Planet(0.72 * au, 12104 * planetScale, new Colour(1, 0.84, 0), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Terrestrial'], 'Venus', null, null),
+        new Planet(1.0 * au, 12756 * planetScale, new Colour(0, 0.72, 0.92), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Terrestrial'], 'Earth', null, null),
     ];
 
     const earth = planets[2];
     planets.push(
-        new Planet(60, 8, new Colour(0.83, 0.83, 0.83), earth, randomAngle(), celestialTypes['satellite'], null, 'Luna')
+        new Planet(earth.radius * 3.0, 20, new Colour(0.83, 0.83, 0.83), earth, randomAngle(), celestialTypes['satellite'], null, 'Luna')
     );
+
+    planets.push(new Planet(1.52 * au, 6792 * planetScale, new Colour(1, 0.27, 0), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Terrestrial'], 'Mars', null, null),
+        new Planet(5.2 * 0.38 * au, 142984 * 0.15 * planetScale, new Colour(0.85, 0.65, 0.13), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Gas Giant'], 'Jupiter', null, null),
+        new Planet(9.54 * 0.31 * au, 120536 * 0.15 * planetScale, new Colour(0.96, 0.64, 0.38), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Gas Giant'], 'Saturn', null,
+            new PlanetaryRing(1.5, 2.5, new Colour(0.96, 0.71, 0.51, 0.75))),
+        new Planet(19.2 * 0.25 * au, 51118 * 0.2 * planetScale, new Colour(0.53, 0.81, 0.92), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Ice Giant'], 'Uranus', null,
+            new PlanetaryRing(1.8, 2.0, new Colour(0.6, 0.9, 1.0, 0.25))),
+        new Planet(30.06 * 0.18 * au, 49528 * 0.2 * planetScale, new Colour(0, 0, 0.55), stars[0], randomAngle(), celestialTypes['planet'], subtypes['Ice Giant'], 'Neptune', null,
+            new PlanetaryRing(2.3, 2.5, new Colour(0.7, 0.7, 0.8, 0.1))));
+
+    const mars = planets[4];
+    const jupiter = planets[5];
+    const halfWidth = (jupiter.distance - mars.distance) * 0.25;
+    const middle = (jupiter.distance + mars.distance) * 0.5;
 
     // Define the Sol System
     const sol = new StarSystem(
@@ -43,7 +50,7 @@ export function createGalaxy() {
         new Vector2D(0, 0),
         stars,
         planets,
-        new AsteroidBelt(3000, 3800, 750, 10)
+        new AsteroidBelt(middle - halfWidth, middle + halfWidth, 1000, 20)
     );
 
     stars = [
